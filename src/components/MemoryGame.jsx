7d1aebe72e726be621fad933d576e5f37cfec678
Prajwal <prajwalnaik159@gmail.com>
2025-08-02 22:10:38 +0530

    Stable version

--- src/components/MemoryGame.jsx
+++ src/components/MemoryGame.jsx
@@ -8,6 +8,7 @@ const MemoryGame = ({ images }) => {
   const [duplicatedImages, setDuplicatedImages] = useState([])
   const [shuffledImages, setShuffledImages] = useState([])
   const [flipped, setFlipped] = useState([])
+  const [selected, setSelected] = useState([])
 
   useEffect(() => {
     setDuplicatedImages([...images, ...images])
@@ -15,20 +16,49 @@ const MemoryGame = ({ images }) => {
   }, [images])
 
   useEffect(() => {
+
     if (duplicatedImages.length > 0) {
-      setShuffledImages(duplicatedImages.map(item => ({ item, sort: Math.random() })).sort((a, b) => (a.sort - b.sort)).map(({ item }) => item))
+      const shuffled = duplicatedImages.map(item => ({ item, sort: Math.random() })).sort((a, b) => (a.sort - b.sort)).map(({ item }) => item)
+
+      setShuffledImages(shuffled)
+      setFlipped(Array(shuffled.length).fill(false))
     }
+
   }, [duplicatedImages])
 
-
   const handleFlip = (index) => {
+    if (flipped[index] === true) return;
+
     const updated = [...flipped];
     updated[index] = !updated[index];
     setFlipped(updated);
-  }
 
-  console.log("duplicatedImages", duplicatedImages)
-  console.log("shuffledImages", shuffledImages)
+    const checkSelected = [...selected, { "value": shuffledImages[index], "index": index, "flipped": true }]
+    setSelected(checkSelected)
+
+    if (checkSelected.length == 2) {
+      const [first, second] = checkSelected;
+      if (first.value === second.value) {
+        console.log("Match found")
+        setSelected([])
+      }
+      else {
+        console.log("No match found")
+        console.log(checkSelected.shift())
+        console.log("checkSelected", checkSelected)
+        const reverted = [...updated]
+        reverted[first.index] = false
+        setFlipped(reverted)
+      }
+    }
+    
+
+  }
+  console.log("outer checkselected", selected)
+  // console.log("duplicatedImages", duplicatedImages)
+  // console.log("shuffledImages", shuffledImages)
+  console.log("flipped", flipped)
+  console.log(selected)
   return (
     <div style={{ position: "relative", width: "100%", height: "100vh" }}>
       <h1>Memory Game</h1>
